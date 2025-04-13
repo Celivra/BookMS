d9943bc61aea3db08533f7ae4f6a0f6f3d7d44c3
Celivra <52xiaozhai@gmail.com>
2025-04-13 19:19:36 +0800

    add filter

--- src/main/java/com/celivra/bookms/Controller/PageController.java
+++ src/main/java/com/celivra/bookms/Controller/PageController.java
@@ -1,10 +1,36 @@
 package com.celivra.bookms.Controller;
 
+import com.celivra.bookms.Entity.User;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PageController {
+    @RequestMapping("/")
+    public String dashboard(HttpServletRequest req) {
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String loginPage(){
+        return "login";
+    }
+
+    @RequestMapping("/doLogin")
+    public String login(
+            @RequestParam String username,
+            @RequestParam String password,
+            HttpServletRequest request ) {
+        System.out.println(username + " " + password);
+        if("admin".equals(username) && "admin".equals(password)){
+            request.getSession().setAttribute("user", new User(username, password));
+            return "redirect:/";
+        }
+        return "login";
+    }
+
     @RequestMapping("/register")
     public String Register(){
         return "register";
---
+++ src/main/java/com/celivra/bookms/Filter/LoginFilter.java
@@ -0,0 +1,32 @@
+package com.celivra.bookms.Filter;
+
+
+import com.celivra.bookms.Entity.User;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class LoginFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req= (HttpServletRequest) servletRequest;
+        HttpServletResponse resp= (HttpServletResponse) servletResponse;
+        String path=req.getRequestURI();
+
+        if(path.contains("/login") || path.contains("/css/") || path.contains("/js/") || path.contains("/register")){
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        User user=(User) req.getSession().getAttribute("user");
+        if(user != null){
+            filterChain.doFilter(servletRequest, servletResponse);
+        } else{
+            resp.sendRedirect("/login");
+        }
+    }
+}

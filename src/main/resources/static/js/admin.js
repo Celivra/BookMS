de15d79854605a0c8587fe32c8cb79522907730f
Celivra <52xiaozhai@gmail.com>
2025-04-17 21:46:59 +0800

    ui update

--- src/main/resources/static/js/admin.js
+++ src/main/resources/static/js/admin.js
@@ -28,6 +28,36 @@ function logout(){
 function closeOverlay(id){
     document.getElementById(id).style.display='none';
 }
+//开启overlay界面
 function toggleAddBook(){
-    document.getElementById('addBookOverlay').style.display = 'flex';
+    document.getElementById('bookOverlay').style.display = 'flex';
+    document.getElementById('addbookh2').style.display = 'block';
+    document.getElementById('addBookForm').style.display = 'block';
+    document.getElementById('changebookh2').style.display = 'none';
+    document.getElementById('changeBookForm').style.display = 'none';
+}
+function toggleChangeBook(bookData){
+
+    //填充内容
+    document.getElementById('changebook-name').innerText = bookData.title;
+    document.getElementById('changebook-author').innerText = bookData.author;
+    document.getElementById('changebook-type').innerText = bookData.type;
+    document.getElementById('changebook-publisher').innerText = bookData.publisher;
+    document.getElementById('changebook-number').innerText = bookData.number;
+    document.getElementById('changebook-desc').innerText = bookData.desc;
+
+    document.getElementById('bookOverlay').style.display = 'flex';
+    document.getElementById('addbookh2').style.display = 'none';
+    document.getElementById('addBookForm').style.display = 'none';
+    document.getElementById('changebookh2').style.display = 'block';
+    document.getElementById('changeBookForm').style.display = 'block';
+}
+function ClickBookCard(el){
+    const title = el.dataset.title;
+    const author = el.dataset.author;
+    const type = el.dataset.type;
+    const publisher = el.dataset.publisher;
+    const number = el.dataset.number;
+    const desc = el.dataset.desc;
+    toggleChangeBook({title, author, type, publisher, number, desc});
 }
